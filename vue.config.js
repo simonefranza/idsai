a989c5d45caea88d02d705601567cf9405212eee
Simone Franza <simone.franza@student.tugraz.at>
2021-02-16 17:45:01 +0100

    maybe now

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,14 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/idsai/'
-    : '/'
-}
-
 const path = require('path')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/idsai/' : '/',
   chainWebpack: config => {
     config.resolve.alias.set(
       'vue$',
       // If using the runtime only build
-      path.resolve(__dirname, 'node_modules/vue/dist/vue.runtime.esm.js')
+      //path.resolve(__dirname, 'node_modules/vue/dist/vue.runtime.esm.js')
       // Or if using full build of Vue (runtime + compiler)
-      //path.resolve(__dirname, 'node_modules/vue/dist/vue.esm.js')
+      path.resolve(__dirname, 'node_modules/vue/dist/vue.esm.js')
     )
   }
 }
