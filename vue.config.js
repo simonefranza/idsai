ca1c7aaa84e5be0329845f782361ce246b299631
Simone Franza <simone.franza@student.tugraz.at>
2021-02-16 17:36:39 +0100

    does github pages work now?

--- vue.config.js
+++ vue.config.js
@@ -11,9 +11,9 @@ module.exports = {
     config.resolve.alias.set(
       'vue$',
       // If using the runtime only build
-      //path.resolve(__dirname, 'node_modules/vue/dist/vue.runtime.esm.js')
+      path.resolve(__dirname, 'node_modules/vue/dist/vue.runtime.esm.js')
       // Or if using full build of Vue (runtime + compiler)
-      path.resolve(__dirname, 'node_modules/vue/dist/vue.esm.js')
+      //path.resolve(__dirname, 'node_modules/vue/dist/vue.esm.js')
     )
   }
 }
